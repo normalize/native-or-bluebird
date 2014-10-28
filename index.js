a721f9565d32cee38f751f95a04ca05ba1a55aa3
Jonathan Ong <jonathanrichardong@gmail.com>
2014-10-27 21:18:11 -0700

    make the error message text a little better

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ module.exports = require('./promise')
 
 /* istanbul ignore next */
 if (!module.exports) {
-  console.error('%s require `Promise`', module.parent.filename)
-  console.error('But neither `bluebird` nor the native `Promise` functions were found.')
+  console.error('The file "%s" requires `Promise`,', module.parent.filename)
+  console.error('but neither `bluebird` nor the native `Promise` implementation were found.')
   console.error('Please install `bluebird` yourself.')
   process.exit(1)
 }
