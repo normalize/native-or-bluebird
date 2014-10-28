fd4983134d9b4c0cdaa86347d2605d87c5c75232
dead_horse <dead_horse@qq.com>
2014-10-28 11:34:39 +0800

    add module.parent.filename in the message

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ module.exports = require('./promise')
 
 /* istanbul ignore next */
 if (!module.exports) {
-  console.error('Neither `bluebird` nor the native `Promise` functions were found.')
+  console.error('%s require `Promise`', module.parent.filename)
+  console.error('But neither `bluebird` nor the native `Promise` functions were found.')
   console.error('Please install `bluebird` yourself.')
   process.exit(1)
 }
